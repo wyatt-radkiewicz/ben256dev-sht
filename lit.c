17df6ac5d787de89ece8b22935ccb5aa0f33b026
ben256dev <benjamin@ben256.com>
2024-10-04 13:22:11 -0600

    sub dir for .sh

--- lit.c
+++ lit.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
    if (argc < 2)
       goto EXIT_NO_COMMAND;
 
-   char argbuff[256];
-
-   for (int i = 0; i < argc; i++)
-      snprintf(argbuff, 256, "%s", argv[i]);
-
-   int status = system(argbuff);
+   int status = -1;
+   if (strcmp(argv[1], "init") == 0)
+      status = system("./sh/init.sh");
 
    return status;
 
